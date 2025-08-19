7025053ee56a85520d12a8596727274950394442
daVinci0304 <bukilbaji@naver.com>
2025-08-19 23:06:24 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,33 @@
-// 서비스 워커 시작 시 즉시 활성화
+let sharedFile = null;
+
+// 앱(클라이언트)이 준비되었다고 메시지를 보내면, 보관중인 파일을 전달합니다.
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.action === 'get-shared-file') {
+    if (sharedFile) {
+      event.source.postMessage({ file: sharedFile });
+      sharedFile = null; // 전달 후에는 비워줍니다.
+    }
+  }
+});
+
 self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  // 공유 타겟 POST 요청을 가로챕니다.
+  // 공유를 통해 앱이 실행되었을 때 POST 요청을 가로챕니다.
   if (event.request.method === 'POST' && event.request.url.includes('index.html')) {
-    event.respondWith(Response.redirect('./')); // 앱의 메인 페이지로 리다이렉트
+    event.respondWith(Response.redirect('./')); // 앱 메인 페이지로 이동시키고,
 
-    // waitUntil을 사용하여 서비스 워커가 종료되기 전에 비동기 작업을 완료합니다.
+    // 파일 처리가 끝날 때까지 서비스워커가 종료되지 않도록 합니다.
     event.waitUntil(async function () {
       const formData = await event.request.formData();
-      const file = formData.get('image'); // 'manifest.json'에서 지정한 name 'image'
-      const clients = await self.clients.matchAll();
-      
-      // 열려있는 모든 클라이언트(앱 창)에 파일을 보냅니다.
-      if (clients && clients.length) {
-        clients[0].postMessage({ file: file });
-      }
+      sharedFile = formData.get('image'); // 파일을 변수에 임시 저장합니다.
     }());
     return;
   }
 
-  // 일반적인 요청은 캐시 또는 네트워크에서 응답합니다.
+  // 그 외의 경우는 캐시에서 파일을 찾거나 네트워크 요청을 보냅니다.
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
